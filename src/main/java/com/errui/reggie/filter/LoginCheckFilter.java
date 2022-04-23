e114219a76a7738be565f64bd336b2719b7f01cd
errui <2909379601@qq.com>
2022-04-23 14:03:26 +0800

    修复无法登录的Bug

--- src/main/java/com/errui/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/errui/reggie/filter/LoginCheckFilter.java
@@ -57,7 +57,7 @@ public class LoginCheckFilter implements Filter {
             return;
         }
 
-        //判断登录状态，如果已登录，则直接放行
+        //判断后台登录状态，如果已登录，则直接放行
         if (request.getSession().getAttribute("employee") != null) {
             log.info("用户已登录，用户id为：{}", request.getSession().getAttribute("employee"));
 
@@ -66,6 +66,17 @@ public class LoginCheckFilter implements Filter {
 
             long id = Thread.currentThread().getId();
             log.info("线程id为:{}", id);
+
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        //判断移动端登录状态，如果已登录，则直接放行
+        if (request.getSession().getAttribute("user") != null) {
+            log.info("用户已登录，用户id为：{}", request.getSession().getAttribute("user"));
+
+            Long userId = (Long) request.getSession().getAttribute("user");
+            BaseContext.setCurrentId(userId);
 
             filterChain.doFilter(request, response);
             return;
