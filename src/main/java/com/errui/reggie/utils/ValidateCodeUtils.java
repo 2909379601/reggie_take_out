76c4c035746db2ce9b6c7c6ff11e41b0a13fb652
errui <2909379601@qq.com>
2022-04-25 15:27:42 +0800

    优化 随机生成验证码工具类

--- src/main/java/com/errui/reggie/utils/ValidateCodeUtils.java
+++ src/main/java/com/errui/reggie/utils/ValidateCodeUtils.java
@@ -1,6 +1,7 @@
 package com.errui.reggie.utils;
 
-import java.util.Random;
+import java.security.SecureRandom;
+
 
 /**
  * @Description: 随机生成验证码工具类
@@ -22,14 +23,14 @@ public class ValidateCodeUtils {
         Integer code = null;
         if (length == 4) {
             //生成随机数，最大为9999
-            code = new Random().nextInt(9999);
+            code = new SecureRandom().nextInt(9999);
             if (code < 1000) {
                 //保证随机数为4位数字
                 code = code + 1000;
             }
         } else if (length == 6) {
             //生成随机数，最大为999999
-            code = new Random().nextInt(999999);
+            code = new SecureRandom().nextInt(999999);
             if (code < 100000) {
                 //保证随机数为6位数字
                 code = code + 100000;
@@ -48,9 +49,9 @@ public class ValidateCodeUtils {
      * @Return: capstr
      */
     public static String generateValidateCode4String(int length) {
-        Random rdm = new Random();
-        String hash1 = Integer.toHexString(rdm.nextInt());
-        String capstr = hash1.substring(0, length);
-        return capstr;
+        SecureRandom secureRandom = new SecureRandom();
+        String hash1 = Integer.toHexString(secureRandom.nextInt());
+        String capStr = hash1.substring(0, length);
+        return capStr;
     }
 }
